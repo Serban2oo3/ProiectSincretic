c318a6c92d74d9e597eeb343f729cc3a878a5472
Serban Ponetchi <102955779+Serban2oo3@users.noreply.github.com>
2024-03-11 22:11:44 +0200

    Update Tema1.ino

--- Tema1.ino
+++ Tema1.ino
@@ -6,29 +6,29 @@ Adafruit_LiquidCrystal lcd(0);
 const int ledPin = 13;
 
 void setup() {
-  pinMode(temperaturaPin, INPUT);  // Setam pinul analogic A0 ca intrare pentru temperatura
-  pinMode(ledPin, OUTPUT); // Setam pinul LED la iesire
-  Serial.begin(9600);  // Se initializeaza comunicarea seriala
+  pinMode(temperaturaPin, INPUT);  
+  pinMode(ledPin, OUTPUT); 
+  Serial.begin(9600);  
   lcd.begin(16, 2);
 }
 
 void loop() {
-  int temperaturaRaw = analogRead(temperaturaPin);  // Citim valoarea analogica
+  int temperaturaRaw = analogRead(temperaturaPin);  
   
-  // Convertim valoarea citita, aproximata
+  
   float temperaturaCelsius = temperaturaRaw * 0.489 - 50; 
   
-  // Afisam temperatura pe ecranul LCD
+ 
   lcd.setCursor(0, 0); 
   lcd.print("Temp: "); 
   lcd.print(temperaturaCelsius);
   lcd.print(" C");
 
-  // Verifica daca a fost primita o comanda
+
   if (Serial.available() > 0) {
     char command = Serial.read();
     
-    //Aprindem/Stingem Led-ul
+    
     if (command == 'A') {
       digitalWrite(ledPin, HIGH);
     } else if (command == 'S') {
